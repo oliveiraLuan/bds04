8332fa15b3cd254ecf2f0151ff091d1c07ac2dd8
Luan de Oliveira <luandeoliveira@MacBook-Air-de-Luan.local>
2023-04-04 22:51:51 -0300

    Added seed, entities and user service

---
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -0,0 +1,99 @@
+package com.devsuperior.bds04.entities;
+
+import org.hibernate.annotations.Fetch;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Table(name = "tb_users")
+@Entity
+public class User implements UserDetails, Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "users_id"),
+            inverseJoinColumns =  @JoinColumn(name =  "roles_id")
+            )
+    private Set<Role> roles = new HashSet<>();
+
+    public User(){
+
+    }
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.
+                stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,17 @@ INSERT INTO tb_event(name, date, url, city_id) VALUES ('Feira do Software', '202
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('CCXP', '2021-04-13', 'https://ccxp.com.br', 1);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Congresso Linux', '2021-05-23', 'https://congressolinux.com.br', 2);
 INSERT INTO tb_event(name, date, url, city_id) VALUES ('Semana Spring React', '2021-05-03', 'https://devsuperior.com.br', 3);
+
+INSERT INTO tb_roles(id, authority) VALUES (1, 'OPERATOR');
+INSERT INTO tb_roles(id, authority) VALUES (2, 'ADMIN');
+
+INSERT INTO tb_users(id, email, password) VALUES (1, 'ana@gmail.com', '123456');
+INSERT INTO tb_users(id, email, password) VALUES (2, 'bob@gmail.com', '123456');
+
+INSERT INTO tb_users_roles(users_id, roles_id) VALUES (1, 1);
+INSERT INTO tb_users_roles(users_id, roles_id) VALUES (2, 1);
+INSERT INTO tb_users_roles(users_id, roles_id) VALUES (2, 2);
+
+
+
+
